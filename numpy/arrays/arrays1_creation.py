7845042816bd9c36b6668cd554a22bb25e2dc749
khushilorish <lorishkhushi@gmail.com>
2025-12-27 13:17:02 +0530

    Arrays creations modified

--- numpy/arrays/arrays1_creation.py
+++ numpy/arrays/arrays1_creation.py
@@ -13,15 +13,15 @@ print("Array of list: ",arr2)
 arr_zeros = np.zeros((2,3)) #array of 2 rows and 3 columns with all values as zero will be created
 print("Array of zeros: ",arr_zeros)
 
-arr_ones = np.ones((3)) #array of 2 rows and 3 columns with all values as one will be created
+arr_ones = np.ones((3)) #flatten array of 3 elements with all values as one will be created
 print("Array of 1's: ",arr_ones)
 
-arr_value = np.full(4,3) #array of 2 rows and 3 columns with all values as 3 will be created
-print("Array of default value: ",arr_value)
+arr_value = np.full(4,3) #flatten array of 4 elements with all values as 3 will be created
+print("Array of default value 3: ",arr_value)
 
 
 #creating a sequenced array
-# arr_sequence = np.arrange(1,11,2) #flatten array with numbers 1,3,5.. till 10 will be created
+arr_sequence = np.arange(1,11,2) #flatten array with numbers 1,3,5.. till 10 will be created
 
 
 #creating identity matrix
