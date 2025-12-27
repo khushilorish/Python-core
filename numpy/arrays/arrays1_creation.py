8cba4677deb4041a5c5417c88d9a79df74629cfc
khushilorish <lorishkhushi@gmail.com>
2025-12-27 13:09:31 +0530

    Arrays creation is explained

---
+++ numpy/arrays/arrays1_creation.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#Basic array creation
+arr1 = np.array([1,2,3])
+print("Simple array: ",arr1)
+
+#converting already existing list into array
+list = [1,2,3,4,5]
+arr2 = np.array(list)
+print("Array of list: ",arr2)
+
+#creating array with default values
+arr_zeros = np.zeros((2,3)) #array of 2 rows and 3 columns with all values as zero will be created
+print("Array of zeros: ",arr_zeros)
+
+arr_ones = np.ones((3)) #array of 2 rows and 3 columns with all values as one will be created
+print("Array of 1's: ",arr_ones)
+
+arr_value = np.full(4,3) #array of 2 rows and 3 columns with all values as 3 will be created
+print("Array of default value: ",arr_value)
+
+
+#creating a sequenced array
+# arr_sequence = np.arrange(1,11,2) #flatten array with numbers 1,3,5.. till 10 will be created
+
+
+#creating identity matrix
+arr_identity = np.eye((4))
+print("Identity array: ",arr_identity)
--- numpy/arrays/arrays_creation.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-#Basic array creation
-arr = np.array([1,2,3])
-print(arr)
