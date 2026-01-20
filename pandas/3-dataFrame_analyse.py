988955f14d01c3d7b979fa8dcb2a5976e660653e
khushilorish <lorishkhushi@gmail.com>
2026-01-20 10:05:11 +0530

    Updated Pandas analysis

--- pandas/3-dataFrame_analyse.py
+++ pandas/3-dataFrame_analyse.py
@@ -26,3 +26,42 @@ print(f'\n\nEmployees whose Salary is greater than 50000: \n{high_salary}')
 #filtering rows with 2 conditions
 filter_row = df[(df["Salary"]>50000) & (df["Age"]>30)]
 print(f'\n\nRows with Salary greater than 50k and age greater than 30: \n{filter_row}')
+
+
+
+data1 = {
+    "Name": ['Arjun', 'Arun', 'Tarun', 'Ankur', 'Tanu', 'Manu'],
+    "Age" : [20,22,21,25,30,31],
+    "Salary": [30000, 40000, 41000, 50000, 60000, 44000],
+    "Performance": [89,90,91,87,86,93],
+    "City": ['Mumbai', 'Agra', 'Chennai', 'Goa', 'Meerut', 'Paris']
+}
+
+df1 = pd.DataFrame(data1)
+
+
+#read first 3 rows of df1 data
+print("First 3 rows:")
+print(df1.head(3)) #if no value passed by default it will show first 5 rows
+
+
+#read last 3 rows of df1 data
+print("\n\nLast 3 rows")
+print(df1.tail(3)) #if no value passed by default it will show last 5 rows
+
+
+#get information about the whole data
+print("\n\nInfo method:")
+print(df1.info())
+
+
+#print Descriptive Statistics of Data
+print("\n\nDescriptive statistics of data:")
+print(df1.describe())
+
+
+#print the shape of data
+print(f'\nShape: {df1.shape}')
+
+#print all the column's name
+print(f'\ncolumns: {df1.columns}')
